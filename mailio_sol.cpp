52a9d93b001e5c51e7121f56bb9ce057a2f01cf7
Rachel <rachelarummer@gmail.com>
2021-06-14 10:46:28 -0400

    debug

--- mailio_sol.cpp
+++ mailio_sol.cpp
@@ -44,7 +44,6 @@ int main()
     cout << "Password: " << password << endl;
     cout << "Recipient email: " << recipient << endl;
 
-    //// debug code.
     //sender = "rachel@wolfcreekmicro.com";
     //password = "123Rachel456";
     //recipient = "rachel@wolfcreekmicro.com";
